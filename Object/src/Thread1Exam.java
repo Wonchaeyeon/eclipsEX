11d25fcfb0ddc0fce30cd5ea9afa582d129b9e10
chaeyeon <zxz8091@naver.com>
2019-06-12 10:08:49 +0900

    add Thread

--- Object/src/Thread1Exam.java
+++ Object/src/Thread1Exam.java
@@ -1,9 +1,21 @@
-
+class Top extends Thread{
+	public void run() {
+		for(int i=0; i<50;i++) {
+			System.out.print(i+"\t");
+		}
+	}
+	
+}
 public class Thread1Exam {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+			System.out.println("프로그램 시작");
+			Top d = new Top();
+			Top d2 = new Top();
+			d.start();
+			d2.start();
+			System.out.println("프로그램 종료");
+				
 	}
 
 }
