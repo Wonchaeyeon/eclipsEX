8dc6509eac1bdf605cee903bb5c7a40ed74ae472
ChaeyeonWon <zxz8091@naver.com>
2019-10-23 08:44:50 +0900

    edit ImageIcon

--- 09.06_GUI1/src/GUI8_ImageIcon.java
+++ 09.06_GUI1/src/GUI8_ImageIcon.java
@@ -8,10 +8,12 @@ public class GUI8_ImageIcon {
         JPanel panel = new JPanel();
 
 
-        ImageIcon iijpg=new ImageIcon("src/img/youtubejpg.jpg");
-        JLabel lbII = new JLabel(iijpg);
-        ImageIcon iipng=new ImageIcon("src/img/youtubepng.png");
-        JLabel btII = new JLabel(iipng);
+        ImageIcon iijpg=new ImageIcon(GUI8_ImageIcon.class.getResource("img/youtubejpg.jpg"));
+        ImageIcon iijpg_adit=new ImageIcon(iijpg.getImage().getScaledInstance(iijpg.getIconWidth()/4,iijpg.getIconHeight()/4,Image.SCALE_DEFAULT));
+        JLabel lbII = new JLabel(iijpg_adit);
+        ImageIcon iipng=new ImageIcon(GUI8_ImageIcon.class.getResource("img/youtubepng.png"));
+        ImageIcon iipng_adit=new ImageIcon(iipng.getImage().getScaledInstance(iipng.getIconWidth()/2,iipng.getIconHeight()/2,Image.SCALE_DEFAULT));
+        JButton btII = new JButton(iipng_adit);
 
         panel.add(lbII);
         panel.add(btII);
